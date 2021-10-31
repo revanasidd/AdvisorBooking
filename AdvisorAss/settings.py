44356f436fb9b7b8fabd653880029412b0716bf8
Revan More <65714331+revanasidd@users.noreply.github.com>
2021-10-31 14:44:44 +0530

    Add files via upload

--- AdvisorAss/settings.py
+++ AdvisorAss/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^^%vnj-gxuefu=ef435gns0@(9c$_e8!!vn5o2zc2u705bv*b$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','advisor1app.herokuapp.com']
 
 
 # Application definition
