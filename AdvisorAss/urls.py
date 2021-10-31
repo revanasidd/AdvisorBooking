003f2bdecdc16838d0e2b38328e913d36d379b6a
Revan More <65714331+revanasidd@users.noreply.github.com>
2021-10-31 14:35:47 +0530

    Add files via upload

--- AdvisorAss/urls.py
+++ AdvisorAss/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.static import serve
+from django.conf.urls import url
 
 from adminApp import views
 
@@ -9,6 +11,8 @@ urlpatterns = [
     path('admin/',admin.site.urls),
     path('users/',include('adminApp.urls')),
     path('advisor/',include('advisor.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
