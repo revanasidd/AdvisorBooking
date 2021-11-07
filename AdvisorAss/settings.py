770c387a3b4815a167d0b11ec4c7d53d1344d082
Revan More <65714331+revanasidd@users.noreply.github.com>
2021-11-07 08:34:18 +0530

    some chnages

--- AdvisorAss/settings.py
+++ AdvisorAss/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,5 +144,7 @@ MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+django_heroku.settings(locals(), logging=False)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
